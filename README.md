0b0daeb683c2222c40ea6193dbc55ae94a25036b
Smilie Pop <arisatominato48@gmail.com>
2025-06-21 19:12:36 +0800

    Changed the bells

---
+++ README.md
@@ -0,0 +1,5 @@
+# ominous-bells
+
+C'mon. You get it. You know what this is for.
+
+![](./public/bells.png)
--- src/components/Mizu5.jsx
+++ src/components/Mizu5.jsx
@@ -3,15 +3,17 @@ import "./Mizu5.css";
 
 const Mizu5 = () => {
     const mizu5 = useRef(null);
-    const bell = useRef(new Audio("./bell.mp3"));
     const [count, setCount] = useState(0);
 
     const toll = () => {
         const image = mizu5.current;
 
         if (image) {
-            const clonedBell = bell.current.cloneNode(true);
-            clonedBell.play();
+            const bellVariants = Math.floor(Math.random() * 2000);
+            console.log(bellVariants);
+            (bellVariants === 1003 | count === 1003)
+                ? new Audio("./bell1.mp3").play()
+                : new Audio("./bell.mp3").play();
 
             image.style.transition = "none";
             image.style.opacity = "1";
@@ -26,7 +28,7 @@ const Mizu5 = () => {
 
     return (
         <>
-            <div className="intro">{count <= 0 ? "Tap anywhere" : ""}</div>
+            {count <= 0 && <div className="intro">Tap anywhere</div>}
             <div className="container" ref={mizu5} onClick={toll}>
                 <img src="./bells.png" className="bells" />
                 <div className="count">{count}</div>
